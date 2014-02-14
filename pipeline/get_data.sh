9cab26767c231ff5965ea7178b840b7ad3b3a25b
arahuja <aahuja11@gmail.com>
2014-02-14 14:35:38 -0500

    adding get data script

---
+++ pipeline/get_data.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Download iedb
+wget http://www.iedb.org/download_manager.php?filename=tcell_compact.zip
+unzip tcell_compact.zip
+
+# Download ensembl data
+
+wget ftp://ftp.ensembl.org/pub/release-74/fasta/homo_sapiens/cds/Homo_sapiens.GRCh37.74.cds.all.fa.gz
+gunzip Homo_sapiens.GRCh37.74.cds.all.fa.gz
+
+wget ftp://ftp.ensembl.org/pub/release-74/fasta/homo_sapiens/pep/Homo_sapiens.GRCh37.74.pep.all.fa.gz
+gunzip Homo_sapiens.GRCh37.74.pep.all.fa.gz
+
+wget ftp://ftp.ensembl.org/pub/release-74/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh37.74.cdna.all.fa.gz
+gunzip Homo_sapiens.GRCh37.74.cdna.all.fa.gz
