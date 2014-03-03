91e5eaa1c0089b235d9e37a1dd0bdbc09804a6ed
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-03-03 13:34:20 -0500

    added newer iedb

--- data/iedb.py
+++ data/iedb.py
@@ -4,13 +4,18 @@ import pandas as pd
 def load_csv(filename = 'tcell_compact.csv', 
              assay_group=None, 
              unique_sequences = True, 
-             noisy_labels = 'majority',
+             noisy_labels = 'majority', # 'majority', 'percent', 'negative', 'positive'
+             
              human = True, 
-             hla_type1 = True,
+             hla_type = None, # 1, 2, or None for neither
+             hla_type1 = False,  # deprecated, kept for old scripts
              exclude_hla_a2 = False,
              only_hla_a2 = False,
              peptide_length = None, 
-             nrows = None):
+             nrows = None,
+             min_count = 0,
+             key_by_allele = False):
+             
   df = pd.read_csv(filename, skipinitialspace=True, nrows = nrows)
   mhc = df['MHC Allele Name']
 
@@ -22,18 +27,22 @@ def load_csv(filename = 'tcell_compact.csv',
   #  or
   #  'Class I,allele undetermined'
   class_1_mhc_mask = mhc.str.contains('Class I,|HLA-[A-C]([0-9]|\*)', na=False).astype('bool')
+  class_2_mhc_mask = mhc.str.contains("Class II,|HLA-D(P|M|O|Q|R)", na=False).astype('bool')
   
   print "Class I MHC Entries", class_1_mhc_mask.sum()
+  print "Class II MHC Entries", class_2_mhc_mask.sum()
   
   # just in case any of the results were from mice or other species, 
   # restrict to humans
-  human_mask = df['Host Organism Name'].str.startswith('Homo sapiens', na=False).astype('bool')
+  human_mask = df['Host Organism Name'].str.startswith('Homo sapiens', na=False).astype('bool') | df["MHC Allele Name"].str.startswith("HLA", na=False).astype('bool')
   
   print "Human entries", human_mask.sum()
   print "Human Class I MHCs", (human_mask & class_1_mhc_mask).sum()
+  print "Human Class II MHCs", (human_mask & class_2_mhc_mask).sum()
   
   null_epitope_seq = df['Epitope Linear Sequence'].isnull()
   print "Dropping %d null sequences" % null_epitope_seq.sum()
+  
   # if have rare or unknown amino acids, drop the sequence
   bad_epitope_seq = df['Epitope Linear Sequence'].str.contains('u|x|j|b|z|U|X|J|B|Z', na=False).astype('bool')
   print "Dropping %d bad sequences" % bad_epitope_seq.sum()
@@ -42,19 +51,20 @@ def load_csv(filename = 'tcell_compact.csv',
   mask = has_epitope_seq
   if human:
     mask &= human_mask
-  if hla_type1:
+  if hla_type1 or hla_type == 1:
     mask &= class_1_mhc_mask 
+  if hla_type == 2:
+    mask &= class_2_mhc_mask
+    
   if assay_group:
     mask &= df['Assay Group'] == assay_group
   
   hla_a2_mask = (mhc == 'HLA-A2') | mhc.str.startswith('HLA-A\*02', na=False)
-  print "HLA A-2 count:", hla_a2_mask.sum() 
   if exclude_hla_a2:
     mask &= ~hla_a2_mask
     
   if only_hla_a2:
     mask &= hla_a2_mask
-  
   
   epitopes = df['Epitope Linear Sequence'].str.upper()
   
@@ -66,44 +76,113 @@ def load_csv(filename = 'tcell_compact.csv',
   
   df = df[mask]
   
+  pos_mask = df['Qualitative Measure'].str.startswith('Positive').astype('bool')
   
-  imm_mask = df['Qualitative Measure'].str.startswith('Positive').astype('bool')
-
-  if noisy_labels == 'majority':
-    groups = imm_mask.groupby(epitopes)
-    imm_mask = groups.mean() >= 0.5
-    non_mask = ~imm_mask
-    imm = imm_mask.index[imm_mask]
-    non = non_mask.index[non_mask] 
+  if key_by_allele:
+    groups = pos_mask.groupby([epitopes, mhc])
   else:
-    non_mask = df['Qualitative Measure'] == 'Negative'
-    imm = epitopes[imm_mask]
-    non = epitopes[non_mask]
+    groups = pos_mask.groupby(epitopes)
   
-  imm_set = set(imm)
-  non_set = set(non)
+  values = groups.mean()
   
-  print "# immunogenic sequences", len(imm)
-  print "# non-immunogenic sequences", len(non)
+  if min_count:
+    counts = groups.count()
+    values = values[counts > min_count]
   
-  noisy_set = imm_set.intersection(non_set)
-  print "# unique IMM", len(imm_set)
-  print "# unique NON", len(non_set)
+  if noisy_labels == 'percent':
+    return values
+  elif noisy_labels == 'majority':
+    pos_mask = values >= 0.5
+  elif noisy_labels == 'positive':
+    pos_mask = values > 0
+  else:
+    pos_mask = values == 1.0
+    
+  neg_mask = ~pos_mask
+  pos = pos_mask.index[pos_mask]
+  neg = neg_mask.index[neg_mask] 
+  
+  pos_set = set(pos)
+  neg_set = set(neg)
+  
+  print "# positive sequences", len(pos)
+  print "# negative sequences", len(neg)
+  
+  noisy_set = pos_set.intersection(neg_set)
+  
+  print "# unique positive", len(pos_set)
+  print "# unique negative", len(neg_set)
   print "# overlap %d (%0.4f)" % (len(noisy_set), \
-      float(len(noisy_set)) / len(imm_set))
+      float(len(noisy_set)) / len(pos_set))
 
   if noisy_labels != 'majority':
     if (noisy_labels == 'drop') or (noisy_labels == 'negative'):
-      imm_set = imm_set.difference(noisy_set)
+      pos_set = pos_set.difference(noisy_set)
     if (noisy_labels == 'drop') or (noisy_labels == 'positive'):
-      non_set = non_set.difference(noisy_set)
+      neg_set = neg_set.difference(noisy_set)
   if unique_sequences:
-    return imm_set, non_set 
+    return pos_set, neg_set 
   else:
-    imm = [epitope for epitope in imm if epitope not in imm_set]
-    non = [epitope for epitope in non if epitope not in non_set]
-    return imm, non 
+    pos = [epitope for epitope in pos if epitope not in pos_set]
+    neg = [epitope for epitope in neg if epitope not in neg_set]
+    return pos, neg 
+
+def load_tcell(assay_group=None, 
+               hla_type = None, # 1, 2, or None for neither
+               peptide_length = None, 
+               nrows = None, 
+               min_count = 0, 
+               key_by_allele = False):
+  return load_csv('tcell_compact.csv', 
+                  assay_group = assay_group, 
+                  noisy_labels = 'percent',
+                  hla_type = hla_type, 
+                  peptide_length = peptide_length, 
+                  nrows = nrows,
+                  min_count = min_count,
+                  key_by_allele = key_by_allele)
+                  
+
+def load_mhc(assay_group=None, 
+             hla_type = None, # 1, 2, or None for neither
+             peptide_length = None, 
+             nrows = None,
+             min_count = 0, 
+             key_by_allele = False):
+  return load_csv('elution_compact.csv', 
+                  assay_group = assay_group, 
+                  noisy_labels = 'percent',
+                  hla_type = hla_type, 
+                  peptide_length = peptide_length, 
+                  nrows = nrows,
+                  min_count = min_count,
+                  key_by_allele = key_by_allele)
+
+def load_tcell_vs_mhc(assay_group=None, 
+                      hla_type = None, # 1, 2, or None for neither
+                      peptide_length = None, 
+                      nrows = None,
+                      min_count = 0, 
+                      key_by_allele = False):
   
+  
+  mhc = load_mhc(assay_group=assay_group,
+                     hla_type = hla_type, 
+                     peptide_length = peptide_length,
+                     nrows = nrows,
+                     min_count = min_count, 
+                     key_by_allele = key_by_allele)
+  tcell = load_tcell(assay_group=assay_group,
+                     hla_type = hla_type, 
+                     peptide_length = peptide_length,
+                     nrows = nrows,
+                     min_count = min_count, 
+                     key_by_allele = key_by_allele)
+                     
+  df_combined = pd.DataFrame({'mhc':mhc, 'tcell':tcell})
+  both = ~(df_combined.mhc.isnull() | df_combined.tcell.isnull())
+  return df_combined[both]
+
 import numpy as np 
 import amino_acid
 from amino_acid import letter_to_index
