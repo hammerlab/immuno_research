26bcde8f730c1c2a9c614eec4448b412c2415554
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-03-22 17:11:31 -0400

    mysterious f scores

--- Mar20_mysterious_neg_acc.py
+++ Mar20_mysterious_neg_acc.py
@@ -14,9 +14,8 @@
 
 
 """
-The CRI cancer epitopes are a good positive validation set
-but need to also make sure we have a low false-positive rate.
-Using non-reactive HIV epitopes as a non-immunogenic peptide set
+A mystery arises wherein we get low accuracy on *both* the positive and
+negative validation sets. What gives?
 """
 import numpy as np
 
@@ -34,15 +33,12 @@ from epitopes import \
 import eval_dataset
 from balanced_ensemble import BalancedEnsembleClassifier
 
-cancer_peptides = cri_tumor_antigens.load_peptides(mhc_class = 1)
-
-print "Positive validation set size: %d" % len(cancer_peptides)
+cancer_peptides = cri_tumor_antigens.load_peptides(mhc_class = None)
 non_immunogenic_hiv_peptides = \
-    hiv_frahm.load_set(max_count = 0)
-
+    hiv_frahm.load_set(max_count = 5)
+print "Positive validation set size: %d" % len(cancer_peptides)
 print "Negative validation set size: %d" % len(non_immunogenic_hiv_peptides)
 
-# self_peptides = reference.load_peptide_set(nrows = 1000)
 
 # sweet over filtering criteria and n-gram transformation
 # parameters and for all parameter combos evaluate
@@ -50,23 +46,6 @@ print "Negative validation set size: %d" % len(non_immunogenic_hiv_peptides)
 # - cross-validation area under ROC curve
 # - accuracy on validation set of cancer peptides
 
-params = []
-aucs = []
-accs = []
-recalls = []
-d = {
-
-    'assay': [],
-    'alphabet' : [],
-    'ngram' : [],
-    'mhc': [],
-    'cv_auc':[],
-    'cv_acc':[],
-    'pos_acc': [],
-    'neg_acc' : [],
-    'f1_score':[],
-    'f_score': [],
-}
 
 
 best_model = None
@@ -74,11 +53,12 @@ best_vectorizer = None
 best_params = None
 
 param_count = 0
-for assay in ('cytotoxicity', None, 'cytokine release IFNg',  ):
-    for alphabet in \
-        ('hp2', 'aromatic2', 'gbmr4', 'hp_vs_aromatic', 'sdm12', None):
-        for max_ngram in (1, 2, 3):
-            for mhc_class in (1,2,None):
+for assay in  ('cytokine release IFNg',):
+    #('cytotoxicity', None, 'cytokine release IFNg',  ):
+    for alphabet in ('hp2',):
+        #('hp2', 'aromatic2', 'gbmr4', 'hp_vs_aromatic', 'sdm12', None):
+        for max_ngram in (3,):#(1, 2, 3):
+            for mhc_class in (2,): #(1,2,None):
                 if alphabet is None:
                     alphabet_dict = None
                     n_letters = 20
@@ -97,10 +77,6 @@ for assay in ('cytotoxicity', None, 'cytokine release IFNg',  ):
                     "%d: Assay = '%s', ngram %s, alphabet %s, mhc_class %s" % \
                     (param_count, assay, max_ngram, alphabet, mhc_class)
                 print param_str
-                d['assay'].append(assay)
-                d['alphabet'].append(alphabet)
-                d['ngram'].append(max_ngram)
-                d['mhc'].append(mhc_class)
 
                 X, Y, vectorizer = iedb.load_tcell_ngrams(
                     assay_group = assay,
@@ -118,29 +94,27 @@ for assay in ('cytotoxicity', None, 'cytokine release IFNg',  ):
                 acc = np.mean(accs)
                 print "CV accuracy %0.4f (std %0.4f)" % \
                     (acc, np.std(accs))
-                d['cv_acc'].append(acc)
 
                 aucs = sklearn.cross_validation.cross_val_score(
                     ensemble, X, Y, cv = 5, scoring='roc_auc')
                 auc = np.mean(aucs)
                 print "CV AUC %0.4f (std %0.4f)" % \
                     (auc, np.std(aucs))
-                d['cv_auc'].append(auc)
 
                 ensemble.fit(X, Y)
 
                 X_pos_test = vectorizer.transform(cancer_peptides)
                 Y_pos_pred = ensemble.predict(X_pos_test)
+                print "Y_pos_pred", Y_pos_pred
                 pos_acc = np.mean(Y_pos_pred)
                 print "Tumor antigen accuracy %0.4f" % (pos_acc,)
-                d['pos_acc'].append(pos_acc)
 
                 X_neg_test = vectorizer.transform(
                     non_immunogenic_hiv_peptides)
                 Y_neg_pred = ensemble.predict(X_neg_test)
+                print Y_neg_pred
                 neg_acc = 1.0 - np.mean(Y_neg_pred)
                 print "Non-immunogenic accuracy %0.4f" % (neg_acc,)
-                d['neg_acc'].append(neg_acc)
 
                 n_pos_pred = np.sum(Y_pos_pred)
                 n_neg_pred = np.sum(Y_neg_pred)
@@ -148,20 +122,12 @@ for assay in ('cytotoxicity', None, 'cytokine release IFNg',  ):
                 recall = pos_acc
                 f1_score = 2 * (precision * recall) / (precision + recall)
                 print "F-1 score: %0.4f" % f1_score
-                d['f1_score'].append(f1_score)
+
 
                 f_half_score = 1.25 * \
                     (precision * recall) / ((0.25 * precision) + recall)
 
                 print "F-0.5 score: %0.4f" % f_half_score
-                d['f_score'].append(f_half_score)
+
                 print "---"
                 print
-
-df = pd.DataFrame(d)
-df = df.sort('f_score', ascending=False)
-print df.to_string()
-with open('Mar19_negative_validation.csv', 'w') as f:
-    df.to_csv(f)
-with open('Mar19_negative_validation.html', 'w') as f:
-    df.to_html(f)
